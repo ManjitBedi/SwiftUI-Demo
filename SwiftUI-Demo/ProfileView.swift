3fd0390fb348f2365bb9eb755f96eeaf826ee66c
Manjit Bedi <dev@manjitbedi.com>
2024-08-31 12:05:52 -0700

    making adjustments

--- SwiftUI-Demo/ProfileView.swift
+++ SwiftUI-Demo/ProfileView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ProfileView: View {
     var body: some View {
         ZStack() {
-            GradientView(colors: [.pink, .blue])
+            GradientView(colors: [.pink, .white])
 
             VStack() {
                 HStack() {
