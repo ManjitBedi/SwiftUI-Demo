62efc3d6c7ef6c05698ce7276126f2e57fd1f468
Manjit Bedi <117043901+ManjitBediBC@users.noreply.github.com>
2024-08-31 11:23:33 -0700

    Initial Commit

---
+++ SwiftUI-Demo/SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DemoApp.swift
+//  SwiftUI-Demo
+//
+//  Created by Manjit Bedi on 2024-08-31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
