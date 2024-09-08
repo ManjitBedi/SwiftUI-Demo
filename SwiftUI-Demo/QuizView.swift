d4d794a53889fbd07542ca5702901c7b218145e5
Manjit Bedi <dev@manjitbedi.com>
2024-09-07 19:05:13 -0700

    basic quiz is working

--- SwiftUI-Demo/QuizView.swift
+++ SwiftUI-Demo/QuizView.swift
@@ -38,30 +38,50 @@ struct QuizView: View {
 struct QuestionView: View {
     let question: QuizQuestion
     let onAnswerSelected: (Int) -> Void
-    @StateObject private var rvm: RiveViewModel
+    @State private var choice = 0
+    @State private var correct = ""
+    @StateObject private var rvm = RiveEventsVM()
 
     init(question: QuizQuestion, onAnswerSelected: @escaping (Int) -> Void) {
         self.question = question
         self.onAnswerSelected = onAnswerSelected
-        _rvm = StateObject(wrappedValue: RiveViewModel(fileName: "quiz-ui", artboardName: "QuizArtboard"))
     }
 
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             rvm.view()
                 .onAppear(){
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                        setQuizContent()
-                    }
+                    setQuizContent()
                 }
+            Text("Event Name: \(rvm.eventName)")
+            Text(correct)
+        }
+        .onChange(of: rvm.eventName) { value, newValue in
+            processEventName(newValue)
         }
         .padding()
     }
 
+    private func processEventName(_ name: String) {
+         // Process the event name here
+        if name == "SubmitEvent" {
+            correct = question.correctAnswerIndex == choice ?
+            "Correct!" : "Incorrect"
+
+        } else if name == "Choice1Event"{
+            choice = 0
+        } else if name == "Choice2Event"{
+            choice = 1
+        } else if name == "Choice3Event"{
+            choice = 2
+        } else if name == "Choice4Event"{
+            choice = 3
+        }
+     }
+
     /// Set the text runs in the Rive view model
     /// Note: the names of the runs need to be flagged as exportin the Rive IDE before exporting a project file!
     func setQuizContent() {
-
         do {
             try rvm.setTextRunValue("QuestionRun", textValue: question.question)
         } catch {
@@ -79,6 +99,35 @@ struct QuestionView: View {
     }
 }
 
+class RiveEventsVM: RiveViewModel {
+    @Published var eventText = ""
+    @Published var eventName = ""
+
+    init() {
+        super.init(fileName: "quiz-ui")
+    }
+
+    // Subscribe to Rive events and this delegate will be invoked
+    @objc func onRiveEventReceived(onRiveEvent riveEvent: RiveEvent) {
+        print("Rive event \(riveEvent.name())")
+        eventName = riveEvent.name()
+        if let openUrlEvent = riveEvent as? RiveOpenUrlEvent {
+            if let url = URL(string: openUrlEvent.url()) {
+                #if os(iOS)
+                UIApplication.shared.open(url)
+                #else
+                NSWorkspace.shared.open(url)
+                #endif
+            }
+        } else if let generalEvent = riveEvent as? RiveGeneralEvent {
+            let genEventProperties = generalEvent.properties();
+            if let msg = genEventProperties["message"] {
+                eventText = msg as! String
+            }
+        }
+
+    }
+}
 
 #Preview {
     QuizView()
